173a59bce85b47eca0ada86e18e8de08eb72872e
yp6497 <yp6497@bs.amalnet.k12.il>
2020-09-09 10:46:55 +0300

    design

--- app/src/main/java/com/example/targil_071/MainActivity.java
+++ app/src/main/java/com/example/targil_071/MainActivity.java
@@ -2,14 +2,102 @@ package com.example.targil_071;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+   EditText edN;
+   float n1,n2,ans;
+   int num;
+   String st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edN=findViewById(R.id.edN);
+    }
+
+    public void credits(View view) {
+        Intent si=new Intent(this,credi.class);
+        si.putExtra("c",ans);
+        startActivity(si);
+    }
+
+    public void clear(View view) {
+        edN.getText().clear();
+
+    }
+
+    public void shave(View view) {
+        st=edN.getText().toString();
+        if(st.isEmpty())
+            Toast.makeText(this,"please enter a number",Toast.LENGTH_SHORT).show();
+        else {
+            edN.getText().clear();
+            n2 = Float.parseFloat(st);
+            if (num == 1) //plus
+                ans = n1 + n2;
+            else if (num == 2) //hisor
+                ans = n1 - n2;
+            else if (num == 3) //kefel
+                ans = n1 * n2;
+            else if (num == 4) //hilok
+                ans = n1 / n2;
+            edN.setText("" + ans);
+        }
+        //edN.getText().clear();
+
+
+    }
+
+    public void hilok(View view) {
+        num=4;
+        st=edN.getText().toString();
+        if(st.isEmpty())
+            Toast.makeText(this,"please enter a number",Toast.LENGTH_SHORT).show();
+        else {
+            edN.getText().clear();
+            n1 = Float.parseFloat(st);
+        }
+
+    }
+
+    public void kefel(View view) {
+        num=3;
+        st=edN.getText().toString();
+        if(st.isEmpty())
+            Toast.makeText(this,"please enter a number",Toast.LENGTH_SHORT).show();
+        else {
+            edN.getText().clear();
+            n1 = Integer.parseInt(st);
+        }
+    }
+
+    public void hisor(View view) {
+        num=2;
+        st=edN.getText().toString();
+        if(st.isEmpty())
+            Toast.makeText(this,"please enter a number",Toast.LENGTH_SHORT).show();
+        else {
+            edN.getText().clear();
+            n1 = Integer.parseInt(st);
+        }
+    }
+
+    public void plus(View view) {
+        num=1;
+        st=edN.getText().toString();
+        if(st.isEmpty())
+          Toast.makeText(this,"please enter a number",Toast.LENGTH_SHORT).show();
+        else {
+            edN.getText().clear();
+            n1 = Integer.parseInt(st);
+        }
     }
 }
---
+++ app/src/main/java/com/example/targil_071/credi.java
@@ -0,0 +1,14 @@
+package com.example.targil_071;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class credi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credi);
+    }
+}
