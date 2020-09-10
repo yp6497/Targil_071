b2c58a4ca59018965c921559de53e07a6a56bf8f
yp6497 <yp6497@bs.amalnet.k12.il>
2020-09-10 15:04:06 +0300

    design

--- app/src/main/java/com/example/targil_071/credi.java
+++ app/src/main/java/com/example/targil_071/credi.java
@@ -2,13 +2,26 @@ package com.example.targil_071;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class credi extends AppCompatActivity {
 
+    Intent gi= getIntent();
+    int ans= gi.getIntExtra("c",1);
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credi);
+        t=findViewById(R.id.t);
+
+        t.setText(ans);
+    }
+
+    public void end(View view) {
+        finish();
     }
 }
