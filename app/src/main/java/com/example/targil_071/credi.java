0985e72f5ef7a83933502958e1cdf699322866c4
yp6497 <yp6497@bs.amalnet.k12.il>
2020-09-11 20:53:46 +0300

    design

--- app/src/main/java/com/example/targil_071/credi.java
+++ app/src/main/java/com/example/targil_071/credi.java
@@ -9,17 +9,25 @@ import android.widget.TextView;
 
 public class credi extends AppCompatActivity {
 
-    Intent gi= getIntent();
-    int ans= gi.getIntExtra("c",1);
-    TextView t;
+    //TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credi);
-        t=findViewById(R.id.t);
+        //Intent gi= getIntent();
+        //
+        //t=findViewById(R.id.t);
+        //t.setText(ans);
+        //t= (TextView) findViewById(R.id.t);
 
-        t.setText(ans);
+        //Intent gi = getIntent();
+        //int x1=gi.getIntExtra("intVariableName", 0);
+        //String t = gi.getStringExtra(Intent.EXTRA_TEXT);
+        //String st=Integer.toString(x1);
+        TextView t = (TextView) findViewById(R.id.t);
+        //t.setText(st);
     }
+
 
     public void end(View view) {
         finish();
