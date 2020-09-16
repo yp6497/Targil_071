1cafdc697842612749cc05e9be182d0f7e6f1aba
yp6497 <yp6497@bs.amalnet.k12.il>
2020-09-16 11:45:05 +0300

    design

--- app/src/main/java/com/example/targil_071/MainActivity.java
+++ app/src/main/java/com/example/targil_071/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void clear(View view) {
         edN.getText().clear();
-
     }
 
     public void shave(View view) {
@@ -43,17 +42,20 @@ public class MainActivity extends AppCompatActivity {
         else {
             edN.getText().clear();
             n2 = Float.parseFloat(st);
-            if (num == 1) //plus
+            if (num == 1)  //plus
                 ans = n1 + n2;
-            else if (num == 2) //hisor
+            else if (num == 2)  //hisor
                 ans = n1 - n2;
             else if (num == 3) //kefel
                 ans = n1 * n2;
             else if (num == 4) //hilok
                 ans = n1 / n2;
+            else if(num==0)
+                ans=n2;
             edN.setText("" + ans);
+            num=0;
+          }
         }
-    }
 
     public void hilok(View view) {
         num=4;
--- app/src/main/java/com/example/targil_071/credi.java
+++ app/src/main/java/com/example/targil_071/credi.java
@@ -27,20 +27,7 @@ public class credi extends AppCompatActivity {
         Intent gi = getIntent();
         String str = gi.getStringExtra("c");
         t.setText("The answer to the last result is: "+str);
-
-        //a=gi.getIntExtra("c", -1);
-        //st = Intent.getStringExtra("m");
-        //st=ed.getText().toString();
-        //a = Integer.parseInt(st);
-        //if(a!='a')
-        //  t.setText(""+st);
-        //else
-        //  t.setText("");
-
-
-
     }
-
 
     public void end(View view) {
         finish();
